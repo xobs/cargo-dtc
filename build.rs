b8c0033d609e13362f5de8c60eebd9e5904167fc
Sean Cross <sean@xobs.io>
2024-07-30 10:24:50 +0800

    build: rebuild files if C source changes
    Signed-off-by: Sean Cross <sean@xobs.io>

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Write};
+use std::{fs::File, io::Write, str::FromStr};
 
 const DTC_FILES: &[&str] = &[
     "dtc/src/checks.c",
@@ -15,15 +15,41 @@ const DTC_FILES: &[&str] = &[
 ];
 
 fn main() {
-    let mut version_file = File::create("dtc/include/version_gen.h").unwrap();
+    let build_target_dir = std::env::var("OUT_DIR").unwrap();
+    let mut version_file = std::path::PathBuf::from_str(build_target_dir.as_str()).unwrap();
+    version_file.push("version_gen.h");
+    let mut version_file = File::create(&version_file).unwrap();
     writeln!(version_file, "#define DTC_VERSION \"{} (rust)\"", env!("CARGO_PKG_VERSION")).unwrap();
     drop(version_file);
 
     let mut build = cc::Build::new();
 
+    for c_src in DTC_FILES {
+        println!("cargo:rerun-if-changed={}", c_src);
+    }
+
+    let include_dir = std::fs::read_dir("dtc/include").unwrap();
+    for entry in include_dir {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.is_file() {
+            println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
+        } else if path.is_dir() {
+            let dir = std::fs::read_dir(path).unwrap();
+            for entry in dir {
+                let entry = entry.unwrap();
+                let path = entry.path();
+                if path.is_file() {
+                    println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
+                }
+            }
+        }
+    }
+
     build
         .include("dtc/include/libfdt")
         .include("dtc/include")
+        .include("target")
         .files(DTC_FILES)
         .warnings(false)
         .define("NO_YAML", None);
