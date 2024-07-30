dfbf41be99896e22e0e825c4fa70440c3ea9db56
Sean Cross <sean@osdyne.com>
2024-07-30 10:12:30 +0800

    dtc: return exit code as part of exit
    Return the exit code of the `dtc` process when the function returns.
    
    Signed-off-by: Sean Cross <sean@osdyne.com>

--- src/dtc.rs
+++ src/dtc.rs
@@ -4,17 +4,38 @@ extern "C" {
     fn dtc_main(argc: core::ffi::c_int, argv: *const *const core::ffi::c_char) -> core::ffi::c_int;
 }
 
-fn main() {
+fn dtc(mut args: &[&str]) -> usize {
     // If this command is run as `cargo dtc`, then the first argument is `cargo`.
     // Strip it off so that the first argument is `dtc`.
-    let mut args = std::env::args().collect::<Vec<String>>();
-    if args.get(1).map(|x| x == "dtc").unwrap_or(false) {
-        args.remove(0);
+    if args.get(1).map(|x| x == &"dtc").unwrap_or(false) {
+        args = &args[1..];
     }
 
     // convert args to a standard C-style `argc, argv`
-    let args_c: Vec<CString> = args.iter().map(|arg| CString::new(arg.as_str()).unwrap()).collect();
+    let args_c: Vec<CString> = args
+        .iter()
+        .map(|arg| CString::new(*arg).unwrap())
+        .collect();
     let args_c: Vec<*const i8> = args_c.iter().map(|arg| arg.as_ptr()).collect();
 
-    unsafe { dtc_main(args_c.len() as _, args_c.as_slice().as_ptr() as _) };
+    unsafe { dtc_main(args_c.len() as _, args_c.as_slice().as_ptr() as _) as _ }
+}
+
+fn main() -> Result<(), usize> {
+    let string_args = std::env::args().collect::<Vec<String>>();
+    let mut str_args: Vec<&str> = vec![];
+    for arg in &string_args {
+        str_args.push(&arg);
+    }
+    let result = dtc(&str_args);
+    if result == 0 {
+        Ok(())
+    } else {
+        Err(result)
+    }
+}
+
+#[test]
+fn test_help() {
+    dtc(&["dtc", "--help"]);
 }
